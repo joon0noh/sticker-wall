df515d949dbc632b5c25203a6a303ce03f7a60bf
coleslaw <joon0noh@gmail.com>
2026-08-20 11:54:07 +0900

    Firebase 연결 및 교사 권한 적용

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase 웹 앱 설정값입니다. 실제 데이터 보호는 Realtime Database 규칙과
+// 교사 UID 권한 검사로 처리합니다.
+export const firebaseConfig = {
+  apiKey: "AIzaSyDlCiLQeF1krkvb3rZ7pMLDtz1MlzZt7yU",
+  authDomain: "ques-c126f.firebaseapp.com",
+  databaseURL: "https://ques-c126f-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "ques-c126f",
+  storageBucket: "ques-c126f.firebasestorage.app",
+  messagingSenderId: "595866786481",
+  appId: "1:595866786481:web:b98e682b6d9d3916b82090",
+  measurementId: "G-N5435V4XBR"
+};
