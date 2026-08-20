be6b71333fcd95179ace2d753ec9c6250e64b5a6
coleslaw <joon0noh@gmail.com>
2026-08-20 11:10:18 +0900

    스티커 담벼락 데모 추가

---
+++ firebase-config.example.js
@@ -0,0 +1,11 @@
+// Firebase Console > Project settings > Your apps > Web app 의 설정값을 복사해 사용하세요.
+// 이 파일을 firebase-config.js로 복사한 뒤 값을 채웁니다.
+export const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
